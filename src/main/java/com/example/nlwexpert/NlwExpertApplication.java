dd64ab0f568097910d63e73d5d30b42827529361
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-02-08 18:14:45 -0300

    setup spring project

---
+++ src/main/java/com/example/nlwexpert/NlwExpertApplication.java
@@ -0,0 +1,13 @@
+package com.example.nlwexpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlwExpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlwExpertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nlwexpert/NlwExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nlwexpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlwExpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
